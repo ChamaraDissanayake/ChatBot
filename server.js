53dd5b7f698f056e70c06f58766f94ba7afd9334
Chamara Dissanayake <dchamarad91@gmail.com>
2025-01-13 00:49:58 +0400

    Cors enabled

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import openai from "./config/open-ai.js";
 import express from "express";
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // In-memory storage of chat history (this can be enhanced with a database for persistence)
